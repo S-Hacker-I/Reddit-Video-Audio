4215cbb9d5548296ab448ce392498ca67001f275
S-Hacker-I <zhackerneh@gmail.com>
2024-09-15 10:48:02 +0300

    first commit

--- app.py
+++ app.py
@@ -4,27 +4,38 @@ import os
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, supports_credentials=True)
 
-@app.route('/download', methods=['POST'])
+@app.route('/download', methods=['POST', 'OPTIONS'])
 def download_video():
-    data = request.json
-    url = data.get('url')
-
-    if not url:
-        return {"error": "URL is required"}, 400
-
-    ydl_opts = {
-        'format': 'bestvideo+bestaudio/best',
-        'outtmpl': 'video.%(ext)s',
-        'noplaylist': True,
-    }
-
-    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-        info_dict = ydl.extract_info(url, download=True)
+    if request.method == 'OPTIONS':
+        return _build_cors_preflight_response()
     
-    video_file = 'video.mp4'
-    return send_file(video_file, as_attachment=True, attachment_filename='video.mp4')
+    if request.method == 'POST':
+        data = request.json
+        url = data.get('url')
+
+        if not url:
+            return {"error": "URL is required"}, 400
+
+        ydl_opts = {
+            'format': 'bestvideo+bestaudio/best',
+            'outtmpl': 'video.%(ext)s',
+            'noplaylist': True,
+        }
+
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            info_dict = ydl.extract_info(url, download=True)
+
+        video_file = 'video.mp4'
+        return send_file(video_file, as_attachment=True, attachment_filename='video.mp4')
+
+def _build_cors_preflight_response():
+    response = app.response_class()
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    response.headers.add("Access-Control-Allow-Headers", "*")
+    response.headers.add("Access-Control-Allow-Methods", "POST, OPTIONS")
+    return response
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 8080))
