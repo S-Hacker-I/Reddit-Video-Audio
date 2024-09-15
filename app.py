284d67081094201c740f457f437e546c260ca8a0
S-Hacker-I <zhackerneh@gmail.com>
2024-09-15 09:11:08 +0300

    first commit

--- app.py
+++ app.py
@@ -1,54 +1,39 @@
 from flask import Flask, request, send_file, jsonify
+from flask_cors import CORS
 import yt_dlp
 import os
-import logging
 
 app = Flask(__name__)
+
+# Enable CORS
+CORS(app)
 
 # Directory to temporarily store video files
 VIDEO_DIR = 'videos'
 if not os.path.exists(VIDEO_DIR):
     os.makedirs(VIDEO_DIR)
 
-# Set up logging
-logging.basicConfig(level=logging.INFO)
-
 @app.route('/download', methods=['POST'])
 def download_video():
-    logging.info('Received request for /download')
     url = request.json.get('url')
     if not url:
         return jsonify({'error': 'No URL provided'}), 400
 
-    logging.info(f"Attempting to download video from URL: {url}")
-
     # Define download options
     ydl_opts = {
-        'format': 'bestvideo+bestaudio/best',
-        'outtmpl': os.path.join(VIDEO_DIR, 'reddit_video.%(ext)s'),
+        'format': 'best',
+        'outtmpl': os.path.join(VIDEO_DIR, 'video.mp4'),
         'noplaylist': True,
-        'quiet': True,
-        'merge_output_format': 'mp4',
-        'postprocessors': [{
-            'key': 'FFmpegVideoConvertor',
-            'preferedformat': 'mp4',
-        }],
+        'quiet': True
     }
 
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         try:
-            info_dict = ydl.extract_info(url, download=True)
-            filename = ydl.prepare_filename(info_dict)
-            logging.info(f"Downloaded video saved to: {filename}")
+            ydl.download([url])
         except Exception as e:
-            logging.error(f"Error downloading video: {str(e)}")
             return jsonify({'error': str(e)}), 500
 
-    filename = os.path.join(VIDEO_DIR, 'reddit_video.mp4')
-    if not os.path.isfile(filename):
-        return jsonify({'error': 'Failed to download video'}), 500
-
-    return send_file(filename, as_attachment=True)
+    return send_file(os.path.join(VIDEO_DIR, 'video.mp4'), as_attachment=True)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
